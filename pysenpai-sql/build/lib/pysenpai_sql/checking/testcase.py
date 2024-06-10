f0e66bbdddc3f4072cef41dfca5d054c3e47b00b
EetuSeppa <eetu.seppanen99@gmail.com>
2024-06-10 15:29:16 +0300

    Test adding to build file

--- pysenpai-sql/build/lib/pysenpai_sql/checking/testcase.py
+++ pysenpai-sql/build/lib/pysenpai_sql/checking/testcase.py
@@ -153,7 +153,7 @@ def run_sql_test_cases(category, test_category, test_target, test_cases, lang,
                     output(msgs.get_msg("PrintStudentOutput", lang), Codes.INFO, output=res)
                     return 0
 
-            case "CREATE":
+            case "CREATE" | "ALTER":
                 ref, res, column_names = test.wrap(test.ref_result, test_target, lang, msgs, test_query=test_query, insert_query=insert_query)
                 if (ref == 0 or res == 0):
                     output(msgs.get_msg("PrintStudentOutput", lang), Codes.INFO, output=res)
---
+++ pysenpai-sql/build/lib/pysenpai_sql/datasetup/datasetup.py
@@ -0,0 +1,36 @@
+import os
+import sqlite3
+
+
+def init_db(sql_file='data.sql'):
+    
+    try:
+        delete_db('mydatabase1.db', 'mydatabase2.db')
+
+        sql_file = open(sql_file, 'r')
+        sql_script = sql_file.read()
+
+        conn = sqlite3.connect("mydatabase1.db")
+        cursor = conn.cursor()
+            
+        cursor.executescript(sql_script)
+
+        conn.commit()
+        conn.close()
+
+        conn2 = sqlite3.connect("mydatabase2.db")
+        cursor2 = conn2.cursor()
+            
+        cursor2.executescript(sql_script)
+
+        conn2.commit()
+        conn2.close()
+    except sqlite3.Error as e:
+        print(e)
+
+def delete_db(db_file1, db_file2):
+    if os.path.exists(db_file1):
+        os.remove(db_file1)
+    if os.path.exists(db_file2):
+        os.remove(db_file2)
+
