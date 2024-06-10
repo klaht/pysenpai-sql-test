ee9652511dd58a9a95d4367359e0d5b3573f9e88
EetuSeppa <eetu.seppanen99@gmail.com>
2024-06-10 13:45:46 +0300

    Added test cases for update queries
    Rest of the queries remain

--- tests/test_sqlTester.py
+++ tests/test_sqlTester.py
@@ -1,7 +1,7 @@
 import pytest
 import subprocess
-import unittest
 import json
+import os
 
 def run_test_case(ans_file, ref_file):
     p = subprocess.Popen(
@@ -13,14 +13,33 @@ def run_test_case(ans_file, ref_file):
     return get_result_from_stdout(str(p.stdout.read()))
 
 def get_result_from_stdout(out_str):
+    print(out_str)
     out_str = "{" + out_str[str.find(out_str, "result") - 1:]
     out_str = str.strip(out_str[:str.rfind(out_str, "}") + 1])
     data = json.loads(out_str)
 
+    print(data)
+
     return data['result']['correct']
 
-
-
-    
 def test_update():
-    assert run_test_case("answer.sql", "reference.sql")
+    for test_case in open_root_directory("../tests/update_test_files"):
+        test_file_path, ref_file_path, should_pass = test_case
+        assert run_test_case(test_file_path, ref_file_path) == should_pass
+
+def open_root_directory(path):
+    test_cases = []
+    for dir in os.listdir(path):
+        for file in os.listdir(path + "/" + dir):
+            #Skip reference file
+            if file == "ref.sql":
+                continue
+
+            test_cases.append([
+                    path + "/" + dir + "/" + file, 
+                    path + "/" + dir + "/ref.sql", 
+                    str.startswith(file, "pass_")
+            ])
+
+    return test_cases
+            
