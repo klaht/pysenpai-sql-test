556e8f1f1f08736fa8d684737695ef7ca676318b
EetuSeppa <eetu.seppanen99@gmail.com>
2024-06-14 10:01:32 +0300

    Added more tests

---
+++ tests/multiple_query_test_files/case2/pass_1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Department (
+    departmentId int NOT NULL,
+    name varchar(255) NOT NULL,
+    leaderId int NOT NULL,
+    PRIMARY KEY (departmentId),
+    FOREIGN KEY (leaderId) REFERENCES Employee(employeeId)
+);
+
+CREATE TABLE Employee (
+    employeeId int NOT NULL,
+    name varchar(255) NOT NULL,
+    departmentId int NOT NULL,
+    PRIMARY KEY (employeeId),
+    FOREIGN KEY (departmentId) REFERENCES Department(departmentId)
+);
+
+INSERT INTO Department VALUES (0, "Test", 0);
