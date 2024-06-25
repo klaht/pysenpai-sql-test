ad11730786d20909907e9f773c721dae45e5b559
EetuSeppa <eetu.seppanen99@gmail.com>
2024-06-25 14:50:14 +0300

    Updated build file

--- pysenpai-sql/build/lib/pysenpai_sql/checking/testcase.py
+++ pysenpai-sql/build/lib/pysenpai_sql/checking/testcase.py
@@ -47,6 +47,7 @@ class SQLTestCase(object):
         }
         if presenters:
             self.presenters.update(presenters)
+        self.feedback_params = {}
     
     def configure_presenters(self, patch):
         self.presenters.update(patch)
@@ -57,13 +58,19 @@ class SQLTestCase(object):
     def present_call(self, target):
         return ""
     
-    def feedback(self, res, descriptions):
-        for eref_result, msg_key in self.eref_results:
-            try:
-                self.validator(eref_result, res, descriptions)
-                yield (msg_key, {})
-            except AssertionError:
-                pass
+    def feedback(self, res, ref):
+        feedback_results = []
+        for setting in open("setting_arguments.txt", "r").readlines():
+            if setting.find("feedback") >= 0:
+                parsed_functions = setting.split("=")[1].split(",")
+                for function in parsed_functions:
+                    try:
+                        feedback_results.append(feedback_functions[function.strip()](res, ref, feedback_params=self.feedback_params)) #Call feedback function
+                    except KeyError:
+                        # TODO: Contact course staff without displaying error? Could be typo in config etc
+                        pass
+            
+                return feedback_results
 
     def parse(self, output):
         return output
@@ -166,16 +173,14 @@ def run_sql_test_cases(category, test_category, test_target, test_cases, lang,
             output(msgs.get_msg("AdditionalTests", lang), Codes.INFO)
                 
             #Extra feedback
-            for msg_key, test_output in test.feedback(res, column_names, ref):
-                if test_output == None:
+            for msg_key, test_output in test.feedback(res, ref):
+                if test_output == None and msg_key:
                     output(msgs.get_msg(msg_key, lang), Codes.INFO)
-                else:
+                elif msg_key:
                     output(msgs.get_msg(msg_key, lang), Codes.INFO, output=test_output)
 
        
         test.teardown()
-        prev_res = res
-        prev_out = test_cases
     
     return grader(test_cases)
 
